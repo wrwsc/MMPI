5389eab232b0e9229a3fd43c780b69b5fc3efd08
wrwsc <destr0yka23@gmail.com>
2025-03-29 12:46:09 +0500

    Add empty page

--- backend/mmpi/urls.py
+++ backend/mmpi/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.shortcuts import render
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -14,7 +15,11 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+def empty_page(request):
+    return render(request, "empty.html")
+
 urlpatterns = [
+    path("", empty_page),
     path("admin/", admin.site.urls),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
