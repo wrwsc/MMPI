672e61bad409cb588b64d2d3c44ada73392262d3
Hoddity <adisulda123@gmail.com>
2025-04-22 11:33:17 +0500

    new design
     Conflicts:
    	.idea/workspace.xml

--- frontend/mmpi-test/src/pages/Home.jsx
+++ frontend/mmpi-test/src/pages/Home.jsx
@@ -1,56 +1,50 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/main.css';
+import backgroundImage from '../img/background.png';
+import titleImage from '../img/testing-title.png';
 
 const Home = () => {
   const navigate = useNavigate();
 
-  const startTest = () => {
-    navigate('/test');
-  };
-
   return (
-    <div className="home-container">
-      <header className="home-header">
-        <h1>ТЕСТИРОВАНИЕ</h1>
-        <h2>MMPI</h2>
-      </header>
-
-      <div className="illustration">
-        <span className="choice left">Да</span>
-        <div className="image-block"></div>
-        <span className="choice right">Нет</span>
+    <div
+      className="home-container"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="title-wrapper">
+        <img src={titleImage} alt="Тестирование MMPI" className="title-image" />
       </div>
 
-      <div className="test-content">
-        <section className="test-description">
-          <h3>Описание теста</h3>
+      <div className="content-wrapper">
+        <div className="description-box">
+          <h2>
+            MMPI<br />
+            (Minnesota Multiphasic Personality Inventory)
+          </h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at erat at lacus hendrerit vulputate. Proin porttitor dui ut aliquam congue. Mauris tempus molestie porta. Donec suscipit mauris id suscipit suscipit. Sed efficitur nibh sed elit dignissim, suscipit dictum dui porttitor. Sed feugiat dolor nibh, at dictum ipsum cursus non. Vivamus arcu tellus, sollicitudin non lacus vitae, aliquam tincidunt turpis. Aliquam feugiat massa elit, in suscipit lacus mattis vitae. Sed placerat orci in eros aliquet, a ultrices ante rutrum. Donec convallis eros felis, sit amet sollicitudin massa interdum eu. Curabitur fringilla eget tellus id accumsan. Aenean vulputate lorem nisl.
+            Миннесотский многоаспектный личностный опросник, разработанный в начале 1940-х годов в университете Миннесоты, - это наиболее изученная и одна из самых популярных психодиагностических методик, предназначенная для исследования индивидуальных особенностей и психических состояний личности.
+            <br /><br />
+            Тест позволяет получить детальный профиль личности, оценить уровень тревожности, депрессии, социальную адаптацию и другие важные психологические параметры.
           </p>
-          <p>
-            Sed fringilla nisi aliquet orci euismod, eget blandit lacus tincidunt. Nam rutrum pulvinar ex, sit amet tincidunt dolor feugiat vel. Praesent blandit nec elit sed aliquam. Maecenas aliquet neque sollicitudin vehicula luctus. Mauris sodales at dolor id iaculis. Aliquam erat volutpat. Nunc elementum ornare augue, eu malesuada ligula aliquam quis. Sed imperdiet vestibulum tellus, eget fermentum felis. Praesent imperdiet ullamcorper nunc, id maximus enim laoreet at. Nullam urna est, laoreet ac venenatis in, laoreet et augue. Donec ipsum massa, ultricies sodales posuere et, venenatis nec magna. In bibendum nisl orci, eu faucibus ipsum efficitur sed. Curabitur eget leo leo. Fusce tellus elit, laoreet in nisl sit amet, tristique scelerisque orci.
-          </p>
-        </section>
-
-        <section className="test-instructions">
-          <h3>Инструкция по прохождению теста</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at erat at lacus hendrerit vulputate. Proin porttitor dui ut aliquam congue. Mauris tempus molestie porta. Donec suscipit mauris id suscipit suscipit. Sed efficitur nibh sed elit dignissim, suscipit dictum dui porttitor. Sed feugiat dolor nibh, at dictum ipsum cursus non. Vivamus arcu tellus, sollicitudin non lacus vitae, aliquam tincidunt turpis. Aliquam feugiat massa elit, in suscipit lacus mattis vitae. Sed placerat orci in eros aliquet, a ultrices ante rutrum. Donec convallis eros felis, sit amet sollicitudin massa interdum eu. Curabitur fringilla eget tellus id accumsan. Aenean vulputate lorem nisl.
-          </p>
-        </section>
+        </div>
       </div>
 
+      <div className="instructions">
+        <h2>Инструкция по прохождению теста</h2>
+        <p>
+          Опросник состоит из 566 утверждений, с каждым из которых вам необходимо либо согласиться, либо нет. Оценивая каждое утверждение, не тратьте много времени на раздумья. Если утверждение верно по отношению к вам в одних ситуациях и неверно в других, выбирайте тот вариант ответа, который больше подходит в настоящий момент.<br /><br />
+          Не волнуйтесь, отвечая на достаточно интимные вопросы, ваши ответы не доступны для чтения в открытом доступе, обработка данных ведётся автоматически.<br /><br />
+          Прохождение опросника занимает немало времени. Убедитесь, что в ближайшее время вас ничего не потревожит и вы сможете дойти до конца тестирования.
+        </p>
 
-
-
-      <div className="test-duration">
-        Примерное время прохождения - 2 часа
       </div>
-
-      <button className="start-test-button" onClick={startTest}>
-        Начать тест
-      </button>
+      <div className="start">
+        <p className="time-note">Примерное время прохождения – 2 часа</p>
+        <button className="start-btn" onClick={() => navigate('/test')}>
+          Начать тест
+        </button>
+      </div>
     </div>
   );
 };
