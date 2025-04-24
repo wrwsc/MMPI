a090916fd298ccc99d9cb6f152d3b909259185b0
wrwsc <destr0yka23@gmail.com>
2025-04-24 20:14:03 +0500

    Add front .vite

---
+++ frontend/mmpi-test/.vite/deps/react-dom.js
@@ -0,0 +1,5 @@
+import {
+  require_react_dom
+} from "./chunk-NGIGROJT.js";
+import "./chunk-Q462PRL3.js";
+export default require_react_dom();
---
+++ frontend/mmpi-test/.vite/deps/react.js
@@ -0,0 +1,4 @@
+import {
+  require_react
+} from "./chunk-Q462PRL3.js";
+export default require_react();
