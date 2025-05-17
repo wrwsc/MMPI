cfa0f55875dd9e76446ae92cdc95496129529da3
vvlasenko <destr0yka23@gmail.com>
2025-05-17 11:17:57 +0500

    KAN-33

--- backend/Api/resetUserAnswers/views.py
+++ backend/Api/resetUserAnswers/views.py
@@ -2,13 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from Dal.models import User, UserAnswer, UserTScore
+from Dal.models import User
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -30,16 +31,8 @@ class ApiResetUserAnswers(APIView):
             user = User.objects.get(user_id=user_id)
         except User.DoesNotExist:
             return Response({"error": "Пользователь не найден"}, status=status.HTTP_404_NOT_FOUND)
-        user_answer = UserAnswer.objects.filter(user=user).first()
 
-        answers_cleared = 0
-        if user_answer:
-            for i in range(1, 567):
-                field_name = f"Вопрос {i}"
-                if hasattr(user_answer, field_name):
-                    setattr(user_answer, field_name, None)
-            user_answer.save()
-            answers_cleared = 1
-        tscores_deleted, _ = UserTScore.objects.filter(user=user).delete()
+        Token.objects.filter(user=user.django_user).delete()
+        user.django_user.delete()
 
-        return Response(status=status.HTTP_200_OK)
+        return Response({"message": "Все данные пользователя удалены"}, status=status.HTTP_200_OK)
